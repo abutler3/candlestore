4b42253b48fb913fe83864f48e1737c5fd506c70
Andrew Butler <abutler3@gmail.com>
2014-08-07 21:07:42 -0500

    Add controllers

--- js/app.js
+++ js/app.js
@@ -7,3 +7,11 @@ App.Router.map(function() {
   this.resource('bio');
 });
 
+
+App.IndexController = Ember.Controller.extend({
+	productsCount: 6,
+	logo: 'http://www.ikea.com/PIAimages/0092092_PE228416_S5.JPG',
+	time: function() {
+		return (new Date()).toDateString()
+	}.property()
+});
