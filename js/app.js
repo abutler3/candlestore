414b91677bbe474a0d5389cf42773e6ebecdbf3a
Andrew Butler <abutler3@gmail.com>
2014-08-09 01:32:19 -0500

    Add Contacts page

--- js/app.js
+++ js/app.js
@@ -6,7 +6,10 @@ App.Router.map(function() {
   this.resource('about');
   this.resource('bio');
 	this.resource('credits', { path: '/thanks' });
-	this.resource('products', { path: '/items' });
+	this.resource('products');
+	this.resource('product', { path: '/products/:title' });
+	this.resource('contacts');
+	this.resource('contact', { path: '/contacts/:name' });
 });
 
 
@@ -30,21 +33,53 @@ App.PRODUCTS = [
 	{
 		title: 'Flint',
 		price: 4,
-		description: 'Flint is',
+		description: 'Flint is a hard, sedimentary cryptocrystalline form of the mineral quartz, categorized as a variety of chert.',
 		isOnSale: true,
 		image: 'http://img1.sunset.timeinc.net/sites/default/files/image/2000/11/candles-carved-m.jpg'
 	},
 	{
 		title: 'Kindling',
 		price: 6,
-		description: 'Easily...',
+		description: 'Easily combustible small sticks or twigs used for starting a fire.',
 		isOnSale: false,
 		image: 'http://www.pkgreenshop.co.uk/ekmps/shops/123pravi/images/real-flame-colour-changing-mood-candle-[3]-804-p.jpg'
 	}
 ];
 
+App.CONTACTS = [
+  {
+    name: 'Giamia',
+    about: 'Although Giamia came from a humble spark of lightning, he quickly grew to be a great craftsman, providing all the warming instruments needed by those close to him.',
+    avatar: 'http://upload.wikimedia.org/wikipedia/commons/7/7f/Laptop_multimedia.jpg'
+  },
+  {
+    name: 'Anostagia',
+    about: 'Knowing there was a need for it, Anostagia drew on her experience and spearheaded the Flint & Flame storefront. In addition to coding the site, she also creates a few products available in the store.',
+    avatar: 'http://upload.wikimedia.org/wikipedia/commons/8/89/Portrait_bw.jpg'
+  }
+];
+
+
 App.ProductsRoute = Ember.Route.extend({
 	model: function() {
 		return App.PRODUCTS;
 	}
+});
+
+App.ContactsRoute = Ember.Route.extend({
+  model: function() {
+    return App.CONTACTS;
+  }
+});
+
+App.ProductRoute = Ember.Route.extend({
+	model: function(params) {
+		return App.PRODUCTS.findBy('title', params.title);
+	}
+});
+
+App.ContactRoute = Ember.Route.extend({
+  model: function(params) {
+    return App.CONTACTS.findBy('name', params.name);
+  }
 });
