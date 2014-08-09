7c098baf363407549828a14c183720ec2129a899
Andrew Butler <abutler3@gmail.com>
2014-08-08 21:18:55 -0500

    Adding products to products page

--- js/app.js
+++ js/app.js
@@ -5,6 +5,8 @@ var App = Ember.Application.create({
 App.Router.map(function() {
   this.resource('about');
   this.resource('bio');
+	this.resource('credits', { path: '/thanks' });
+	this.resource('products', { path: '/items' });
 });
 
 
@@ -14,4 +16,35 @@ App.IndexController = Ember.Controller.extend({
 	time: function() {
 		return (new Date()).toDateString()
 	}.property()
+});
+
+App.AboutController = Ember.Controller.extend({
+  contactName: 'Amber',
+  avatar: 'http://upload.wikimedia.org/wikipedia/commons/7/7f/Laptop_multimedia.jpg',
+  open: function() {
+    return ((new Date()).getDay() === 0) ? "Closed" : "Open";
+  }.property()
+});
+
+App.PRODUCTS = [
+	{
+		title: 'Flint',
+		price: 4,
+		description: 'Flint is',
+		isOnSale: true,
+		image: 'http://img1.sunset.timeinc.net/sites/default/files/image/2000/11/candles-carved-m.jpg'
+	},
+	{
+		title: 'Kindling',
+		price: 6,
+		description: 'Easily...',
+		isOnSale: false,
+		image: 'http://www.pkgreenshop.co.uk/ekmps/shops/123pravi/images/real-flame-colour-changing-mood-candle-[3]-804-p.jpg'
+	}
+];
+
+App.ProductsRoute = Ember.Route.extend({
+	model: function() {
+		return App.PRODUCTS;
+	}
 });
