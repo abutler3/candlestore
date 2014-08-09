8029de6611f44f46437d8843af695665bd7ca0ed
Andrew Butler <abutler3@gmail.com>
2014-08-09 16:43:15 -0500

    Change data to fixtures

--- js/app.js
+++ js/app.js
@@ -7,10 +7,10 @@ App.Router.map(function() {
   this.resource('bio');
 	this.resource('credits', { path: '/thanks' });
 	this.resource('products', function() {
-		this.resource('product', { path: '/:title' });
+		this.resource('product', { path: '/:product_id' });
 	});
   this.resource('contacts', function() {
-  	this.resource('contact', { path: '/:name' });
+  	this.resource('contact', { path: '/:contact_id' });
   });
 });
 
@@ -31,8 +31,20 @@ App.ContactsIndexController = Ember.Controller.extend({
   }.property()
 });
 
-App.PRODUCTS = [
+App.ApplicationAdapter = DS.FixtureAdapter.extend();
+
+
+App.Product = DS.Model.extend({
+	title: DS.attr('string'),
+  price: DS.attr('number'),
+  description: DS.attr('string'),
+  isOnSale: DS.attr('boolean'),
+  image: DS.attr('string')
+});
+
+App.Product.FIXTURES = [
 	{
+		id: 1,
 		title: 'Flint',
 		price: 4,
 		description: 'Flint is a hard, sedimentary cryptocrystalline form of the mineral quartz, categorized as a variety of chert.',
@@ -40,6 +52,7 @@ App.PRODUCTS = [
 		image: 'http://img1.sunset.timeinc.net/sites/default/files/image/2000/11/candles-carved-m.jpg'
 	},
 	{
+		id: 2,
 		title: 'Kindling',
 		price: 6,
 		description: 'Easily combustible small sticks or twigs used for starting a fire.',
@@ -48,13 +61,22 @@ App.PRODUCTS = [
 	}
 ];
 
-App.CONTACTS = [
+
+App.Contact = DS.Model.extend({
+	name: DS.attr('string'),
+  about: DS.attr('string'),
+  avatar: DS.attr('string')
+});
+
+App.Contact.FIXTURES = [
   {
+		id: 1,
     name: 'Giamia',
     about: 'Although Giamia came from a humble spark of lightning, he quickly grew to be a great craftsman, providing all the warming instruments needed by those close to him.',
     avatar: 'http://upload.wikimedia.org/wikipedia/commons/7/7f/Laptop_multimedia.jpg'
   },
   {
+		id: 2,
     name: 'Anostagia',
     about: 'Knowing there was a need for it, Anostagia drew on her experience and spearheaded the Flint & Flame storefront. In addition to coding the site, she also creates a few products available in the store.',
     avatar: 'http://upload.wikimedia.org/wikipedia/commons/8/89/Portrait_bw.jpg'
@@ -64,24 +86,20 @@ App.CONTACTS = [
 
 App.ProductsRoute = Ember.Route.extend({
 	model: function() {
-		return App.PRODUCTS;
+		return this.store.findAll('product');
 	}
 });
 
 App.ContactsRoute = Ember.Route.extend({
   model: function() {
-    return App.CONTACTS;
+    return this.store.findAll('contact');
   }
 });
 
-App.ProductRoute = Ember.Route.extend({
-	model: function(params) {
-		return App.PRODUCTS.findBy('title', params.title);
-	}
-});
+
 
 App.ContactRoute = Ember.Route.extend({
   model: function(params) {
-    return App.CONTACTS.findBy('name', params.name);
+     return this.store.find('contact', params.contact_id);;
   }
 });
